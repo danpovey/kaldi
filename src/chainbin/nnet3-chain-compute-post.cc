e436bc960b3f419876967f52bcc6a92af05b3258
freewym <freewym@gmail.com>
2019-11-02 22:44:59 -0400

    fix

--- src/chainbin/nnet3-chain-compute-post.cc
+++ src/chainbin/nnet3-chain-compute-post.cc
@@ -201,16 +201,11 @@ int main(int argc, char *argv[]) {
       // swap.
       CuMatrix<BaseFloat> gpu_nnet_output;
       gpu_nnet_output.Swap(&matrix);
-      bool all_boundary = (RandInt(0, 1) == 0);
-      KALDI_LOG << "All-boundary = " << std::boolalpha << all_boundary;
+
       Matrix<BaseFloat> boundary_mask(4, 1);
-      if (all_boundary) {
-        boundary_mask.Row(0).Set(1.0);
-        boundary_mask.Row(2).Set(1.0);
-      } else { // all not-boundary, i.e. all split-point.
-        boundary_mask.Row(1).Set(1.0);
-        boundary_mask.Row(3).Set(1.0);
-      }
+      // always is real boundary
+      boundary_mask.Row(0).Set(1.0);
+      boundary_mask.Row(2).Set(1.0);
 
 
       chain::DenominatorComputation den_computation(
