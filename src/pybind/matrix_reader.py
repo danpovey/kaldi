b8cce3c18d19ffd39ed7d5203225e935998ecf2b
Dongji Gao <dgao@b12.clsp.jhu.edu>
2019-11-16 20:39:49 -0500

    modified matrix_reader.py

--- src/pybind/matrix_reader.py
+++ src/pybind/matrix_reader.py
@@ -45,3 +45,45 @@ class ReaderIterator():
 
     def __next__(self):
         return self.iterator.__next__()
+
+class RandomAccessVectorReader(kp.RandomAccessBaseFloatMatrixReader_Vector):
+    def __init__(self, rspecifier = None):
+        if not rspecifier:
+            kp.RandomAccessBaseFloatMatrixReader_Vector.__init__(self)      
+        else:
+            kp.RandomAccessBaseFloatMatrixReader_Vector.__init__(self, rspecifier)      
+
+    def __getitem__(self, key):
+        if not self.HasKey(key):
+            raise KeyError("{} does not exits.".format(key))
+        else:
+            return self.Value(key)
+
+    def __contains__(self, key):
+        return self.HasKey(key)
+
+class RandomAccessMatrixReader(kp.RandomAccessBaseFloatMatrixReader_Matrix):
+    def __init__(self, rspecifier = None):
+        if not rspecifier:
+            kp.RandomAccessBaseFloatMatrixReader_Matrix.__init__(self)      
+        else:
+            kp.RandomAccessBaseFloatMatrixReader_Matrix.__init__(self, rspecifier)      
+
+    def __getitem__(self, key):
+        if not self.HasKey(key):
+            raise KeyError("{} does not exits.".format(key))
+        else:
+            return self.Value(key)
+
+    def __contains__(self, key):
+        return self.HasKey(key)
+
+class ReaderDict():
+    def __init__(self, reader_class):
+        self.dict = reader_class
+
+    def __getitem__(self, key):
+        return self.dict.__getitem__(key)
+
+    def __contains__(self, key):
+        return self.dict.__contains__(key)
